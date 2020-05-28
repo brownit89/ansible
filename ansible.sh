181f99c0922aaf6dfadbb11655218e40469aa5de
root <root@localhost.localdomain>
2020-05-27 22:50:36 -0700

    updated script's default locations

--- ansible.sh
+++ ansible.sh
@@ -5,24 +5,23 @@ then
     sudo mkdir ~/.ssh
     echo "created new .ssh directory"
 else
-    sudo rm -rf /home/centosuser/.ssh
+    sudo rm -rf ~/.ssh
     echo "removed existing .ssh directory"
-    sudo mkdir /home/centosuser/.ssh
+    sudo mkdir ~/.ssh
     echo "reinstalled .ssh directory"
 fi
 
 yes "y" | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
-yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.20
-yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
-yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
-yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
+yes "y" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.20
+yes "y" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
+yes "y" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
+yes "y" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install epel-release
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y epel-release
 sudo yum install -y ansible
 sudo ansible mynodes -i ./hosts.ini -b -m raw -a "apk -U add python3"
-#sudo ansible mynodes -i ./hosts.ini -m ping
 sudo ansible-playbook webserver.yaml -i ./hosts.ini
 
 curl 192.168.1.20
