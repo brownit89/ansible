cb02f29d7c712e265602dbc7c722ea40d81e751e
root <root@localhost.localdomain>
2020-05-27 22:44:35 -0700

    switch fedora with epel-rel

--- ansible.sh
+++ ansible.sh
@@ -18,9 +18,10 @@ yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
-#sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 echo "installing epel-release"
 sudo yum --disablerepo="epel" -y update ca-certificates install -y epel-release
+sudo rm -rf /var/cache/yum
 sudo yum install -y ansible
 sudo ansible mynodes -i ./hosts.ini -b -m raw -a "apk -U add python3"
 sudo ansible-playbook webserver.yaml -i ./hosts.ini
