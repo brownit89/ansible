d3799eb27485125907556985d7642c75127ca238
CentOS User <centosuser@localhost.localdomain>
2020-05-27 22:19:37 -0700

    working second install

--- ansible.sh
+++ ansible.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -d ".ssh" ];
+if [ ! -d "~/.ssh" ];
 then
-    sudo mkdir /home/centosuser/.ssh
+    sudo mkdir ~/.ssh
     echo "created new .ssh directory"
 else
     sudo rm -rf /home/centosuser/.ssh
@@ -11,16 +11,19 @@ else
     echo "reinstalled .ssh directory"
 fi
 
-yes "y" | ssh-keygen -t rsa -N "" -f /home/centosuser/.ssh/id_rsa
+yes "y" | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.20
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
-ansible mynodes -i ./hosts.ini -b -m raw -a "apk -U add python3"
-ansible mynodes -i ./hosts.ini -m ping
-ansible-playbook webserver.yaml -i ./hosts.ini
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install epel-release
+sudo yum install -y ansible
+sudo ansible mynodes -i ./hosts.ini -b -m raw -a "apk -U add python3"
+#sudo ansible mynodes -i ./hosts.ini -m ping
+sudo ansible-playbook webserver.yaml -i ./hosts.ini
 
 curl 192.168.1.20
 curl 192.168.1.21
