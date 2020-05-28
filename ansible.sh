48a94d2d523b35d876dff994a2db5faa58714678
root <root@localhost.localdomain>
2020-05-27 22:57:20 -0700

    fixed epel error

--- ansible.sh
+++ ansible.sh
@@ -18,9 +18,9 @@ yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
 yes "yes" | ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y epel-release
-sudo yum --disablerepo=epel/x86_64 -y update ca-certificates
+#sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+echo "installing epel-release"
+sudo yum --disablerepo="epel" -y update ca-certificates install -y epel-release
 sudo yum install -y ansible
 sudo ansible mynodes -i ./hosts.ini -b -m raw -a "apk -U add python3"
 sudo ansible-playbook webserver.yaml -i ./hosts.ini
